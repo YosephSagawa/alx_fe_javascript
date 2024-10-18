e17c232cb946ae0e67f534f0c586ac398828ebca
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-18 17:25:18 +0300

    Added the styles.css file and modified the script.js file

---
+++ dom_manipulation/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const quoteDisplay = document.getElementById("quotedisplay");
+    const newQuote = document.getElementById("newQuote");
+});
