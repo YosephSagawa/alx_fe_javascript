da1c2c126cccddfdc00c6c2bb42910cbbc825ec0
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-18 17:40:09 +0300

    Added the quotes array and the function declarations

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const quoteDisplay = document.getElementById("quotedisplay");
     const newQuote = document.getElementById("newQuote");
+
+    const quotes = [{category: "loveQuote", text: "I love JavaScript"},
+                    {category: "aestheticQuote", text: "Live your life the way you want to."},
+                    {category: "psychologyQuote", text: "No soul is burdened more than it could bear"}];
+
+    function showRandomQuote() {
+
+    };
+
+    function createAddQuoteForm(){
+
+    };
 });
